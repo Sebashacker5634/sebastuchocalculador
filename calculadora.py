f81a12cb9e4638c93867274c26589bb22e95ccd3
Sebastian <juangp_16@hotmail.com>
2025-06-29 09:32:06 -0500

    first commit

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+print ("esto es una calculadora bro")
+print ("escriba un numero")
+a= int (input ())
+b= int (input ())
+c= a+b
+d = a-b
+e = a * b
+print ( "la suma es ", c)
+print ( "la resta es" , d )
+print ( "la multiplicacion es", e)
+print ( "la potencia es", a ** b)
+if b != 0:
+    print ( "la division es", a / b)
+    print ( "el modulo es", a % b)
+    print ( "la division entera es", a // b)
+else:
+    print("No se puede dividir, sacar módulo ni división entera por cero.")
+    print("No se puede dividir, sacar módulo ni división entera por cero.")
+    print ( "la potencia es", a ** b)
